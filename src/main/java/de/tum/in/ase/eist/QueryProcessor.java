6a257a3e5b8c8d2b2209016864d0c6bf990354cb
Philippe Zimmermann <ge47qax@mytum.de>
2022-07-13 15:09:24 +0200

    1

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,9 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "MyTeam";
-        } else { // TODO extend the programm here
+        } else if(query.contains("2269d5d0")){
+            return "493";
+        }else { // TODO extend the programm here
             return "";
         }
     }
